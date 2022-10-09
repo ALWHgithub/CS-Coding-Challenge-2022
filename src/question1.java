99d220472f737f04ee704120b1549edb27e52c9a
ALWHgithub <104885208+ALWHgithub@users.noreply.github.com>
2022-10-09 13:48:28 +0800

    Create question1.java

---
+++ src/question1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+class Solution {
+
+    static int solution(String files) {
+        // Write solution here
+        return -1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        String str = scanner.nextLine();
+        scanner.close();
+
+        int answer = solution(str);
+        System.out.println(answer);
+    }
+}
